a02b5794727ed05cb7d77a6368a5dbfd18bb8338
alexrmsu <al.magna@alumnos.duoc.cl>
2022-01-30 15:24:08 -0300

    generated all components needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapsComponent } from './maps/maps.component';
+import { MiniMapComponent } from './mini-map/mini-map.component';
+import { FullscreenComponent } from './fullscreen/fullscreen.component';
+import { MarkersComponent } from './markers/markers.component';
+import { ZoomRangeComponent } from './zoom-range/zoom-range.component';
+import { PropertiesComponent } from './properties/properties.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapsComponent,
+    MiniMapComponent,
+    FullscreenComponent,
+    MarkersComponent,
+    ZoomRangeComponent,
+    PropertiesComponent
   ],
   imports: [
     BrowserModule,
